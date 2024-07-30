3f97266172f44b810c58d7b6ba9dd82405394290
shashwa7-dev <shashwa7.dev@gmail.com>
2024-07-30 14:26:04 +0530

    token expiry set to 100 for testing

--- app/api/refresh-token/route.tsx
+++ app/api/refresh-token/route.tsx
@@ -51,7 +51,7 @@ export async function GET() {
     return NextResponse.json(
       {
         access_token: tokenData.access_token,
-        expires_in: expiresIn,
+        expires_in: expiresIn - 3500,
       },
       {
         status: 200,
