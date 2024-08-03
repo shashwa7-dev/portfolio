c02f05f275be7b238abb40d811f3a5c4857ffcc6
shashwa7-dev <shashwa7.dev@gmail.com>
2024-08-04 01:21:42 +0530

    initial loader added!

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,29 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import CurrentState from "./components/CurrentState";
 import Footer from "./components/Footer";
 import Intro from "./components/Intro";
+import Loading from "./loading";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <div
-      className="container p-5 lg:p-10 lg:pb-5 flex flex-col gap-6 h-full"
+      className="container p-5 lg:p-10 lg:pb-5 flex flex-col gap-6 h-full opacity-0 animate-fadeIn"
       style={{ zIndex: 1 }}
     >
       <Intro />
