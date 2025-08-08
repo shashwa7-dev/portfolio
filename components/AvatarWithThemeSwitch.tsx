2307b85288ee91013675027db23d2f25c0e368e4
shashwa7-dev <shashwa7.dev@gmail.com>
2025-08-09 02:37:14 +0530

    avatar fix

--- components/AvatarWithThemeSwitch.tsx
+++ components/AvatarWithThemeSwitch.tsx
@@ -7,19 +7,19 @@ export default function AvatarWithThemeSwitch() {
   const { isDarkMode, toggleDarkMode } = useDarkMode();
 
   return (
-    <div className="relative">
+    <div className="relative h-fit">
       <div className="w-[80px] h-[80px] overflow-hidden rounded-full border-4">
         <Avatar isDarkMode={isDarkMode} />{" "}
       </div>
       <div className="flex items-center w-fit absolute -bottom-3 left-0 right-0 mx-auto rounded-xl border-4 overflow-hidden bg-card p-1 gap-2">
         <div
           onClick={toggleDarkMode}
-          className="w-4 h-4 cursor-pointer transition-transform duration-500 ease-in-out hover:scale-110"
+          className="w-3 h-3 cursor-pointer transition-transform duration-500 ease-in-out hover:scale-110"
         >
           {isDarkMode ? (
-            <SVGS.Moon className="w-full h-full object-cover rotate-0 scale-100 transition-all duration-500 ease-in-out" />
+            <SVGS.Moon className="w-full h-full" />
           ) : (
-            <SVGS.Sun className="w-full h-full object-cover rotate-180 scale-75 transition-all duration-500 ease-in-out" />
+            <SVGS.Sun className="w-full h-full" />
           )}
         </div>
       </div>
